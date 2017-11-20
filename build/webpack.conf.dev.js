845bb0e239b370c8c06e79e09086094a65e5fe6f
shanghaitao <359310444@qq.com>
2017-11-20 11:15:33 +0800

    up pkg

--- build/webpack.conf.dev.js
+++ build/webpack.conf.dev.js
@@ -6,11 +6,6 @@ var baseWebpackConfig = require('./webpack.conf.base')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
-// add hot-reload related code to entry chunks
-Object.keys(baseWebpackConfig.entry).forEach(function (name) {
-  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
-})
-
 module.exports = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
